69167a7ea05759380ecf8179c6667ec99978b13e
rajibhasan01 <hasan.rajib1996@gmail.com>
2024-01-20 16:07:09 +0600

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,17 @@
-import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 4204,
-  },
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 3000,
+  strictPort: true,
+ },
+ server: {
+  port: 3000,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:8080",
+ },
 });
